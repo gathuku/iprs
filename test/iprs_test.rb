b00f3da395e0126284aa1bf3c32feef072845c6e
Moses Gathuku <mosesgathuku95@gmail.com>
2021-11-19 10:16:25 +0300

    bundle gem

---
+++ test/iprs_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class IprsTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Iprs::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
